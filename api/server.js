0fc81e620420a4748bc014cdca5c5825bdf91540
Kazbonfim <zcry.games@gmail.com>
2025-01-06 09:54:31 -0300

    :rocket: Ajuste de URL

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ module.exports = (req, res) => {
     // Criar o servidor HTTP e passar a requisição para o Express
     const server = http.createServer(app);
 
+    
     // Redireciona a requisição para o Express
     server.emit('request', req, res);
 };
