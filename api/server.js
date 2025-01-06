ad7ae1f6a790303d98fd1928bc9e10111d116d68
Kazbonfim <zcry.games@gmail.com>
2025-01-06 09:45:37 -0300

    :rocket: Deploy

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const app = require('../app');
+const http = require('http');
+
+module.exports = (req, res) => {
+    // Criar o servidor HTTP e passar a requisição para o Express
+    const server = http.createServer(app);
+
+    // Redireciona a requisição para o Express
+    server.emit('request', req, res);
+};
