129e8ebda2cfe00b34d7850e35beeb6404a06342
Kazbonfim <zcry.games@gmail.com>
2025-01-06 10:34:30 -0300

    :rocket: deploy

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv = require('dotenv');
+const cors = require('cors')
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: `https://jsonwtk.vercel.app`, credentials: true }));
 
 app.use('/', indexRouter);
 
